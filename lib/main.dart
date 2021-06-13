11f74b99e1f141d7c752487dc3d584a348144559
Tomasz.Michlewski <tomasz.michlewski@gmail.com>
2021-06-13 23:13:44 +0200

    project structure

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,4 @@
+import 'package:face_scaner/ui/app.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.blue),
-      home: Container(),
-    );
-  }
-}
+void main() => runApp(App());
