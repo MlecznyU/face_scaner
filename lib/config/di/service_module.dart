d45617b36037f756ad3a038597abd6d127511455
Tomasz.Michlewski <tomasz.michlewski@gmail.com>
2021-06-20 20:17:44 +0200

    orientation_service.dart added

--- lib/config/di/service_module.dart
+++ lib/config/di/service_module.dart
@@ -1,9 +1,14 @@
 import 'package:face_scaner/data/face_scan/face_scan_service_dummy.dart';
+import 'package:face_scaner/data/services_impl/default_orientation_service.dart';
 import 'package:face_scaner/domain/face_scan/face_scan_service.dart';
+import 'package:face_scaner/domain/orientation_service.dart';
 import 'package:injectable/injectable.dart';
 
 @module
 abstract class ServiceModule {
   @Injectable(as: FaceScanService)
   FaceScanServiceDummy get faceScanBloc;
+
+  @Injectable(as: OrientationService)
+  DefaultOrientationService get defaultOrientationService;
 }
---
+++ lib/data/services_impl/default_orientation_service.dart
@@ -0,0 +1,25 @@
+import 'package:face_scaner/domain/orientation_service.dart';
+import 'package:flutter/services.dart';
+
+class DefaultOrientationService implements OrientationService {
+  @override
+  Future<void> setOrientation(List<AppOrientation> orientations) async {
+    final deviceOrientations = orientations.map((orientation) {
+      switch (orientation) {
+        case AppOrientation.portraitUp:
+          return DeviceOrientation.portraitUp;
+
+        case AppOrientation.landscapeLeft:
+          return DeviceOrientation.landscapeLeft;
+
+        case AppOrientation.portraitDown:
+          return DeviceOrientation.portraitDown;
+
+        case AppOrientation.landscapeRight:
+          return DeviceOrientation.landscapeRight;
+      }
+    }).toList();
+
+    await SystemChrome.setPreferredOrientations(deviceOrientations);
+  }
+}
